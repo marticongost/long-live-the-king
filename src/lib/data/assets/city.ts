ee842edc9a784456e49dbdbd1c2969505846dddb
marticongost <marticongost@gmail.com>
2026-08-29 20:46:19 +0200

    Split city into village and city, make city more powerful

--- src/lib/data/assets/city.ts
+++ src/lib/data/assets/city.ts
@@ -3,12 +3,12 @@ import type { GoalData } from '$lib/models/cards';
 export default {
 	title: 'Ciutat',
 	properties: ['land'],
-	discardBonus: { power: 2 },
+	discardBonus: { power: 2, gold: 2 },
 	capabilities: [
 		{
 			title: 'Centre de poder',
 			type: 'action',
-			effects: 'Guanyar {power 1}, {gold 1}, {intrigue 1} o {strength 1}. Límit un cop per torn.'
+			effects: 'Guanyar {power 2}, {gold 2}, {intrigue 2} o {strength 2}. Límit un cop per torn.'
 		}
 	]
 } satisfies GoalData;
---
+++ src/lib/data/assets/village.ts
@@ -0,0 +1,14 @@
+import type { GoalData } from '$lib/models/cards';
+
+export default {
+	title: 'Poble',
+	properties: ['land'],
+	discardBonus: { power: 2 },
+	capabilities: [
+		{
+			title: 'Nucli de població',
+			type: 'action',
+			effects: 'Guanyar {power 1}, {gold 1}, {intrigue 1} o {strength 1}. Límit un cop per torn.'
+		}
+	]
+} satisfies GoalData;
