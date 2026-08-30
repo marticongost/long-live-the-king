f8011d86bb16d6427eefd5059174bd6554a441a9
marticongost <marticongost@gmail.com>
2026-08-30 14:28:17 +0200

    Add the redouble-efforts tactic

---
+++ src/lib/data/tactics/redouble-efforts.ts
@@ -0,0 +1,15 @@
+import { type TacticData } from '$lib/models/cards';
+
+export default {
+	title: 'Doblar els esforços',
+	discardBonus: { power: 1 },
+	capabilities: [
+		{
+			title: 'Aplicar',
+			type: 'action',
+			cost: { power: 1 },
+			effects:
+				"Escull una acció d'una {tactic} de la teva ma. Paga el seu cost, i executa els seus efectes dues vegades. Descarta la carta."
+		}
+	]
+} satisfies TacticData;
