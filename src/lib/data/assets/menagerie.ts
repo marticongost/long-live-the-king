0d9941269706c9715c434037f5877e4828e68b46
marticongost <marticongost@gmail.com>
2026-09-02 17:39:18 +0200

    Add the menagerie card

---
+++ src/lib/data/assets/menagerie.ts
@@ -0,0 +1,14 @@
+import type { AssetData } from '$lib/models/cards';
+
+export default {
+	title: 'Col·lecció de feres',
+	properties: ['land'],
+	discardBonus: { gold: 3 },
+	capabilities: [
+		{
+			type: 'reaction',
+			trigger: 'gameEnd',
+			effects: 'Guanyar {prestige 1}.'
+		}
+	]
+} satisfies AssetData;
